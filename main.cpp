48b8e672e37541af237c4b7086f1c1f818a2e3d7
Zangi99 <92801992+Zangi99@users.noreply.github.com>
2021-11-29 16:03:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+using namespace std;
 
-int main(){
-  return 0;
+int Funzione (char a){
+    char b; bool flag;
+if ((a>=65)&&(a<=90)){
+flag=true;
+b=a+32;
 }
+else if ((a>=97)&&(a<=122)){
+flag=true;
+b=a-32;
+}
+else{flag=false;}
+
+if (flag==true){
+cout<<b<<endl;}
+else{cout<<"errore"<<endl;}
+return flag;
+}
+
+int main () {
+    char a;
+    cin>>a;
+    Funzione(a);
+
+    return 0;
+}
+
